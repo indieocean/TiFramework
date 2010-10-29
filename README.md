3b005a9ba87f33aea0b06299e5e73b6dca71f748
Rick Blalock <rickblalock@mac.com>
2010-10-29 01:02:38 -0400

    Initial commit. Woohoo!

---
+++ README.md
@@ -0,0 +1,78 @@
+[TiFramework](http://xidlabs.com/) - Wrapper framework for Titanium
+================================
+
+What is it?
+---------------------------------------
+TiFramework is a helper framework for Appcelerator's Titanium.  It's currently in early (yeah really early) alpha.  Actually, if there's was a letter before alpha then it'd probably be that.
+
+TiFramework condenses the Titanium API further and allows you to chain commands together in a jQuery-esque fashion. This will allow you to keep your code condensed and stay within the familiar methods jQuery to accomplish similar things.
+
+Goals
+--------------
+
+The following are the goals of the project:
+
+* **`Tidy Code`**: The biggest reason for this framework is to keep the code condensed and organized.  Allowing many of the TI API to be chain-able is the most obvious representation of this goal.
+
+* **`Familiarity`**: This library should not take away you away from learning how Titanium works but rather help you develop in Titanium by using familiar methods and approaches similar to jQuery and Mootools.
+
+* **`OOP`**: The end goal is not to just have a few wrapper methods that allow others to chain UI objects together but to have an OO framework which can be used for an entire app.  This includes things as models, controllers, event driven, etc.
+
+If you want to make it better
+-----------------------------
+I'm definitely not the smartest on Github so if you see a way to make the framework much better, add more functionality, etc.  Please contribute!  As the framework grows I'd love to post some info on best practices, proper naming and coding conventions, etc.  This thing will evolve as long as others are contributing and helping improve it!
+
+
+How do I use it?
+-----------------------------
+
+Here are some examples on how to use the framework.  More will be documented in the wiki as support for the Titanium API is fleshed out.
+
+In this case we're just re-assigning the TiFrameowrk namespace to $ sign for a faux-jQuery appearance
+    
+    var $ = TiFramework;
+
+Create a new window.  Notice .context is appended.  That way the variable is actually
+storing the window object itself and not the full TiFramework class.  Probably
+should be rewritten so this isn't necessary.
+
+NOTE:  It will take two types for it's arguement.  You can pass it a string such as "window", "label", "view", etc.  This will create a new UI object of that name.  Or you can pass an already created object as the argument.  Below we're creating two new windows.  If I wanted to reference one of the windows below I would use **'$(main_window)'** or **'$(second_window)'**
+
+    var main_window 	= $('window').context;
+    var second_window 	= $('window').context;
+
+Create a new tabgroup, add new tabs, assign tabs the appropriate window, open the tab group
+
+    $('tabgroup')
+    		.tab({title: 'Examples', window: main_window})
+    		.tab({title: 'Second Tab', window: second_window})
+    	.open();
+
+Just some dummy data for examples
+
+    var data = [
+    	{ title: 'Test' },
+    	{ title: 'Test', hasDetail: true },
+    	{ title: 'Test', hasChild: true }
+    ];
+
+Put a table with data in an already created winow
+
+    $(main_window).tableView({data: data});
+
+Put a new label on an existing window
+
+    $(main_window)
+    	.label({text: 'New Label', zIndex: 9999, color: '#333', textAlign: 'center', height: 50, bottom: 10})
+    	.click(function(e) {
+    		alert('Label Clicked');
+    	});
+
+Create a new view, add a label to it, append the view to the existing window, add a click function
+
+    $('view').label({text: 'New Label', color: '#eee', textAlign: 'center'})
+    	.appendTo(second_window)
+    	.click(function(e) {
+    		alert('View Clicked');
+    	});
+
---
+++ tiframework.js
@@ -0,0 +1,207 @@
+/*
+ * TiFramework for Titanium
+ *
+ * Copyright 2010, Rick Blalock
+ * Licensed under the MIT
+ * Copyright (c) 2010 Rick Blalock
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+// Set current version and codename constants.  This is helpful if
+// you need to work with different versions
+// @TODO Not implemented yet
+var TI_FRAMEWORK_VERSION 	= '0.1', 
+	TI_FRAMEWORK_CODENAME 	= 'Zergling';
+
+/**
+ * The Framework class
+ *
+ * @param mixed context
+ */	
+function TiFramework(context) {
+	// Return the new framework class
+	return new TiFramework.prototype(context);
+};
+
+TiFramework.prototype = function(context) {
+	// Make the context accessible
+	this.context = context;
+	
+	// Return the object even if no context is provided
+	if(!this.context) {
+		return this;
+	}
+	
+	/** 
+	 * Handle new context
+	 * @TODO create more
+	 */
+	if(typeof this.context === 'string') {
+		// Determine the string context
+		switch(this.context) {
+			// @TODO Should these use the UI helper methods below and open automatically or defer to the open() method?
+			// The latter is more verbose but gives more control on when UI elements are drawn out
+			case 'window':
+				this.context = Ti.UI.createWindow();
+				break;
+				
+			case 'view':
+				this.context = Ti.UI.createView();
+				break;
+
+			case 'table':
+				this.context = Ti.UI.createTableView();
+				break;				
+				
+			case 'label':
+				this.context = Ti.UI.createLabel();
+				break;	
+				
+			case 'tabgroup':
+				this.context = Ti.UI.createTabGroup();
+				break;							
+		}
+	}
+
+
+/** --- EVENT HELPERS --- */
+
+	/** Click event helper
+	 *
+	 * @param callback
+	 */
+	this.click = function(callback) {		
+		this.context.addEventListener('click', function(e) {
+			callback(e);
+		});
+
+		return this;
+	};
+
+/** --- UI UTILITIES --- */
+
+	/** Attach data to the context (usually tableView)
+	 *
+	 * @param object data
+	 */
+	this.setData = function(data) {		
+		this.context.data = data;
+	
+		return this;
+	};
+
+	/** Attach options to the context
+	 *
+	 * @param object opts
+	 */
+	this.setOpts = function(opts) {		
+		// @TODO For some reason this doesn't work at all.  Something with the prop var and type?
+		for(var prop in opts) {
+			this.context.prop = opts[prop];
+		}
+	
+		return this;
+	};	
+
+	/** Append current context to the indicated element
+	 *
+	 * @param object
+	 */
+	this.appendTo = function(element) {
+		element.add(this.context);
+	
+		return this;
+	};
+	
+	/** Append passed object to current context
+	 *
+	 * @param object
+	 */
+	this.append = function(element) {
+		this.context.add(element);
+	
+		return this;
+	};	
+	
+	/** Open the current context
+	 *
+	 * @param object opts
+	 */
+	this.open = function(opts) {
+		this.context.open(opts);
+		
+		return this;
+	};	
+
+/** --- UI ELEMENTS --- */
+	
+	/** Create a general view object
+	 *
+	 * @param object opts
+	 */
+	this.view = function(opts) {
+		var view = Ti.UI.createView(opts);
+	
+		this.context.add(view);
+	
+		return this;
+	};
+
+	/** Create and add a tableview
+	 *
+	 * @param object opts
+	 */
+	this.tableView = function(opts) {
+		var tableview = Ti.UI.createTableView(opts);
+		
+		this.context.add(tableview);
+		
+		return this;
+	};	
+	
+	/** Create and add a label
+	 *
+	 * @param object opts
+	 */
+	this.label = function(opts) {
+		var label = Ti.UI.createLabel(opts);
+		
+		this.context.add(label);
+		
+		return this;
+	};
+	
+	/** Create and add a tab to a tab group
+	 *
+	 * @param object opts
+	 */
+	this.tab = function(opts) {
+		var tab = Ti.UI.createTab(opts);
+		
+		this.context.addTab(tab);
+		
+		return this;
+	};	
+	
+	
+	/** Return the global object */
+	return this;
+};
